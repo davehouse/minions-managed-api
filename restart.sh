18976db60346a7b438873de0e9252b6fd55fde7e
Rob Thijssen <rthijssen@gmail.com>
2018-11-08 09:38:54 +0200

    adapt restart script for cron usage

--- restart.sh
+++ restart.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-git pull
-npm install
-forever restartall
+if git checkout master && git fetch origin master && [ `git rev-list HEAD...origin/master --count` != 0 ] && git merge origin/master; then
+  npm install
+  forever restartall
+else
+  echo "current revision is up to date. restart not required."
+fi
