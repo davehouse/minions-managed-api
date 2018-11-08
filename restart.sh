f1eb8770e07e22b315f7fd0502e307805b0e3ec5
Rob Thijssen <rthijssen@gmail.com>
2018-11-08 11:28:51 +0200

    adapt restart script to be called from anywhere

--- restart.sh
+++ restart.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-if git checkout master && git fetch origin master && [ `git rev-list HEAD...origin/master --count` != 0 ] && git merge origin/master; then
-  npm install
+repo_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+if git --git-dir=${repo_dir}/.git --work-tree=${repo_dir} checkout master && git --git-dir=${repo_dir}/.git --work-tree=${repo_dir} fetch origin master && [ `git --git-dir=${repo_dir}/.git --work-tree=${repo_dir} rev-list HEAD...origin/master --count` != 0 ] && git --git-dir=${repo_dir}/.git --work-tree=${repo_dir} merge origin/master; then
+  npm --prefix ${repo_dir} install ${repo_dir}
   forever restartall
 else
   echo "current revision is up to date. restart not required."
