7730d2ed1882d9be02d4f0d627c96809837ab73e
Dave House <dhouse@mozilla.com>
2018-11-12 12:55:09 -0700

    match "No task claimed"

--- server.js
+++ server.js
@@ -396,7 +396,31 @@ db.once('open', function() {
           : {};
       switch (event.program.toLowerCase()) {
         case 'generic-worker':
-          if (event.message.match(/Running task https/i)) {
+          if (event.message.match(/No task claimed/i)) {
+            var task = {
+              id: event.message.split('#')[1].split('/')[0],
+              started: new Date(event.received_at)
+            };
+            Minion.findOneAndUpdate(
+              {
+                _id: id
+              },
+              {
+                instanceId: hostname,
+                workerType: workerType,
+                dataCenter: dataCenter,
+                ipAddress: event.source_ip,
+                lastEvent: (new Date())
+              },
+              { upsert: true },
+              function(error, model) {
+                console.log(workerType + ' ' + hostname + ' - idle');
+                if (error) {
+                  return console.error(error);
+                }
+              }
+            );
+          } else if (event.message.match(/Running task https/i)) {
             var task = {
               id: event.message.split('#')[1].split('/')[0],
               started: new Date(event.received_at)
